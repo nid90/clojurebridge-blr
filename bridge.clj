3cd629e2ac27f6e0dde07d5f90a89d1dd3585059
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2019-03-25 23:17:27 +0530

    Adds collections content.

--- bridge.clj
+++ bridge.clj
@@ -26,3 +26,29 @@ inc
       z (* 2 z)]
   (println z)
   x)
+{:first       "Harry"
+ :middle     "James"
+ :last       "Potter"
+ :occupation "Auror"}
+(assoc {:first "Hermione"} :last "Granger")
+(dissoc {:first "Hermione" :last "Granger"} :last)
+(merge {:first "Hermione"} {:last "Granger"})
+(get {:first "Hermione" :last "Granger"} :first)
+(get {:first "Hermione"} :last)
+(get {:first "Hermione"} :last "NA")
+(keys {:first "Hermione" :last "Granger"})
+(vals {:first "Hermione" :last "Granger"})
+(def hello {:count 1 :words "hello"})
+(assoc hello :words "bye")
+(update hello :count inc)
+(update hello :words str ", world")
+
+(def wizards [{:name  "Harry Pooter"
+               :house "Gryffindor"}
+              {:name  "Draco Malfoy"
+               :house "Slytherin"}])
+
+(def houses {:gryffindor {:colors ["scarlet" "gold"]
+                          :points 200}
+             :slytherin  {:colors ["green" "silver"]
+                          :points 150}})
