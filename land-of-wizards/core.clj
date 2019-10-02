05901f3397eb3b707d13b4a00bc9e29383ce6667
Murtaza Akbari <murtazasakbari@gmail.com>
2019-10-02 20:56:51 +0530

    Minor changes to names of defs, and corrections in comments

--- land-of-wizards/core.clj
+++ land-of-wizards/core.clj
@@ -7,10 +7,9 @@
 (defn describe-location [location nodes]
   (get nodes location))
 
-(describe-location :living-room nodes)
+#_ (describe-location :living-room nodes)
 
-(comment
- "The `describe-location` seems pretty awkward in several different ways. First of all, why are the variables for location and map being passed as parameters, instead of just reading the global variables directly? The reason is that Lispers often like to write code in the Functional Programming Style (To be clear, this is completely unrelated in any way to the concept called \"procedural programming\" or \"structural programming\"). In this style, the goal is to write functions that are referentially transaparent. An expression is called referentially transparent if it can be replaced with its corresponding value without changing the program's behavior, and to achive that:
+(comment "The `describe-location` seems pretty awkward in several different ways. First of all, why are the variables for location and map being passed as parameters, instead of just reading the global variables directly? The reason is that Lispers often like to write code in the Functional Programming Style (To be clear, this is completely unrelated in any way to the concept called \"procedural programming\" or \"structural programming\"). In this style, the goal is to write functions that are referentially transaparent. An expression is called referentially transparent if it can be replaced with its corresponding value without changing the program's behavior, and to achive that:
 
 1.  You only read variables that are passed into the function or are created by the function (So you don't read any global variables)
 2.  You never change the value of a variable that has already been set (So no incrementing variables or other such foolishness)
@@ -23,37 +22,36 @@
     (str "There is a " (name obstacle) " going "
          (name path) " from here.")))
 
-(describe-path edges :living-room :attic)
+#_ (describe-path edges :living-room :attic)
 
 (comment "Now there's a function to describe all paths in and out of wizard's current location. This function uses another common functional programming technique: The use of Higher Order Functions - This means that the map function is taking other functions as parameters so that they can call them themselves - map simply applies another function to every object in the collection (in this case a list) returned by `(keys (location edges))`, basically causing all paths to be changed into pretty descriptions procided by the describe-path function.")
 
-(defn describe-all-paths
-  [edges location]
+(defn describe-all-paths [edges location]
   (str/join " " (map #(describe-path edges location %) (keys (location edges)))))
 
-(describe-all-paths edges :living-room)
+#_ (describe-all-paths edges :living-room)
 
 (comment "A function to get all the objects at a given location. This is a function that does just that.")
 
-(defn objects-at-location
-  [location object-locations]
-  (get object-locations location))
+(defn objects-at [location location->objects]
+  (get location->objects location))
 
-(objects-at-location :living-room object-locations)
+#_ (objects-at :living-room location->objects)
 
-(comment "To describe all the objects at a given location, the `objects-at-location` function isfirst called to get a collection (in this case a vector) of all the objects at the location, and then invoke the function stored in `object-whereabouts` on all elements of the collection by using `map`. `map` returns a lazy sequence of elements (which in this case are strings). The strings in this sequence are the `join`ed to form a single string.")
+(comment "To describe all the objects at a given location, the `objects-at-location` function isfirst called to get a collection (in this case a vector) of all the objects at the location, and then invoke the function stored as `describe-object-whereabouts` on all elements of the collection by using `map`. `map` returns a lazy sequence of elements (which in this case are strings). The strings in this sequence are then `join`ed to form a single string.")
 
-(defn describe-objects [location object-locations]
-  (let [object-whereabouts #(str "You see a " (name %) " on the floor.")]
-    (str/join " " (map object-whereabouts (objects-at-location location object-locations)))))
+(defn describe-objects-at [location location->objects]
+  (let [object-whereabouts-description #(str "You see a " (name %) " on the floor.")
+        objects-at-location (objects-at location location->objects)]
+    (str/join " " (map object-whereabouts-description objects-at-location))))
 
-(describe-objects :living-room object-locations)
+#_ (describe-objects :living-room location->objects)
 
 (comment "`look` ties all these descriptor functions into a single, easy command called `look`` that uses the global variables (therefore this function is not in the Functional Style) to feed all the descriptor functions and describes everything")
 
 (defn look [location]
   (str (describe-location location nodes) " "
        (describe-all-paths location edges) " "
-       (describe-objects location object-locations)))
+       (describe-objects-at location location->objects)))
 
-(look :living-room)
+#_ (look :living-room)
